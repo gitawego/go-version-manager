2794da8b9bdc9f4b687908efbefc1c31bd51b057
Hongbo LU <gitawego@gmail.com>
2022-04-21 21:39:29 +0200

    updated

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # go-version-manager
+
+install
+
+```bash
+bash < <(curl -Ks -S -L https://raw.githubusercontent.com/gitawego/go-version-manager/main/installer.sh)
+```
--- installer.sh
+++ installer.sh
@@ -3,3 +3,9 @@
 echo "creating folder .go in home"
 
 mkdir -p ~/.go
+
+cd ~
+
+curl -kL -O https://raw.githubusercontent.com/gitawego/go-version-manager/main/go-vm.fnc.sh
+
+echo "source ./go-vm.fnc.sh" >> ~/.bashrc
